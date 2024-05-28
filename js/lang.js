76b4a7b7e5ebb586555814802bf626d47bb192c4
Artem Ushakov <umd900@mail.ru>
2024-05-29 02:51:46 +0700

    haha

---
+++ js/lang.js
@@ -0,0 +1,106 @@
+let arrLang = {
+    'ru': {
+        'menu': 'Меню',
+        'main': 'Главная',
+        'about': 'Обо мне',
+        'welup': 'Я TheFreo,',
+        'welun': ' Я начинающий программист. Люблю сидеть дома и смотреть аниме :-) Сейчас изучаю C++ и JavaScript.',
+        'bio': 'Кто я?',
+        'skills': 'Навыки',
+        'devices': 'Девайсы',
+        'pc': 'Основной ПК',
+        'lap': 'Ноутбук (MateBook D14)',
+        'os': 'Система:',
+        'cpu': 'Процессор:',
+        'gpu': 'Видеокарта:',
+        'ram': 'Оперативка:',
+        'name': 'Имя: Артём',
+        'brith': 'День рождения: 15.06.2005 (Осталось ',
+        'brith2': ' дней)',
+        'country': 'Страна: Россия (мечтаю переехать в Японию)',
+        'city': 'Город: Новосибирск (Родной: Сургут)',
+        'stud': 'Учусь: СибГУТИ',
+        'langu': 'Языки: Русский, Английский (30%), Украинский (70%)',
+        'listen': 'Слушаю: Рок (Любой), IDM, Ambient',
+        'cont': 'Контакты',
+        'morning': 'Доброе утро!',
+        'afternoon': 'Добрый день!',
+        'evening': 'Добрый вечер!',
+        'night': 'Доброй ночи!'
+    },
+    'en': {
+        'menu': 'Menu',
+        'main': 'Main',
+        'about': 'About me',
+        'welup': "I'm TheFreo,",
+        'welun': "I'm an aspiring programmer. I like to sit at home and watch anime :-) At the moment I am studying C++ and JavaScript.",
+        'bio': 'Who Am I?',
+        'skills': 'Skills',
+        'devices': 'Devices',
+        'pc': 'Main PC',
+        'lap': 'Laptop (MateBook D14)',
+        'os': 'OS:',
+        'cpu': 'CPU:',
+        'gpu': 'GPU:',
+        'ram': 'RAM:',
+        'name': 'Name: Artem',
+        'brith': 'Brithday: 15.06.2005 (through ',
+        'brith2': ' days)',
+        'country': 'Counrty: Russia (dream: Japan)',
+        'city': 'City: Novosibirsk (Hometowm: Surgut)',
+        'stud': 'Studying: SibSUTIS',
+        'langu': 'Languages: Russia, English (30%), Ukrainian (70%)',
+        'listen': 'Listen: Rock (Any), IDM, Ambient',
+        'cont': 'Contacts',
+        'morning': 'Good morning!',
+        'afternoon': 'Good afternoon!',
+        'evening': 'Good evening!',
+        'night': 'Good night!'
+    }
+};
+
+// Language switcher
+var lang;
+$(document).ready(function () {
+    lang = "ru";
+    $("lang").each(function (index, element) {
+        $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+});
+
+$(".translate").click(function () {
+    lang = $(this).attr("id");
+    $(".lang").each(function (index, element) {
+        $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+});
+
+//Hello!
+function setTime() {
+    var dateTime = new Date();
+    var hour = dateTime.getHours();
+    var message = "";
+
+    if (hour < 6) {
+        message = "night";
+        $("#timehello").attr('key', 'night');
+    } else if (hour < 12) {
+        message = "morning";
+        $("#timehello").attr('key', 'morning');
+    } else if (hour < 18) {
+        message = "afternoon";
+        $("#timehello").attr('key', 'afternoon');
+    } else if (hour < 24) {
+        message = "evening";
+        $("#timehello").attr('key', 'evening');
+    }
+    console.log(hour);
+
+    if (message !== "") {
+        $("#timehello").html("Good " + message + "!");
+    }
+}
+setTime();
+
+
+
---
+++ js/main.js
@@ -0,0 +1,68 @@
+// Burger button
+$('.burger').click(function () {
+    $('.menu').toggleClass('open');
+    $('.burger').toggleClass('burger-open');
+})
+
+// Dark theme 
+$('.themebtn').click(function () {
+    $('.wrapper').toggleClass('darkthm');
+    $('.header_line').toggleClass('darkline');
+    $('.themebtn').toggleClass('darkbtn');
+    $('.burger').toggleClass('darkbtn');
+    $('.avatar img').toggleClass('avatardark');
+})
+
+// Tabs 
+let button = document.getElementsByClassName('but'),
+    tabContent = document.getElementsByClassName('tab-content');
+button[0].classList.add('active');
+tabContent[0].style.display = 'block';
+
+function city(e, city) {
+    let i;
+    for (i = 0; i < button.length; i++) {
+        tabContent[i].style.display = 'none';
+        button[i].classList.remove('active');
+    }
+    document.getElementById(city).style.display = 'block';
+    e.currentTarget.classList.add('active');
+}
+
+// Close burger
+$('.but').click(function () {
+    $('.menu').toggleClass('open');
+    $('.burger').toggleClass('burger-open');
+})
+
+// Brithday
+
+var now = new Date();
+var current_year = now.getFullYear();
+var next_year = current_year;
+
+var target_date = new Date("Jun 15, " + next_year).getTime();
+
+var days;
+
+var $days = document.getElementById("d");
+
+function update() {
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+
+    days = parseInt(seconds_left / 86400);
+
+    $days.innerHTML = pad(days, 2);
+}
+
+update();
+
+setInterval(update, 1000);
+
+function pad(num, size) {
+    var s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+};
+
